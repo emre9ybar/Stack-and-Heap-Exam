6056efe936dcd916dd22e2cddedda10292d6a235
emre aybar <aybaremre5@gmail.com>
2024-05-04 18:08:41 +0300

    3 adet

--- Exam.java
+++ Exam.java
@@ -1,5 +1,9 @@
 public class Exam {
     public static void main(String[] args) {
         System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println("helleo");
     }
 }
